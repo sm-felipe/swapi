b0b8e95e92da71ae095e52012798f916904d7e7f
femello <femello@uolinc.com>
2019-07-28 13:34:38 -0300

    service para planeta

--- src/main/java/com/ame/swapi/repository/PlanetRepository.java
+++ src/main/java/com/ame/swapi/repository/PlanetRepository.java
@@ -1,9 +1,13 @@
 package com.ame.swapi.repository;
 
 import com.ame.swapi.entity.PlanetEntity;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlanetRepository extends CrudRepository<PlanetEntity, Long> {
+
+    Optional<PlanetEntity> findByName(String name);
+
 }
---
+++ src/main/java/com/ame/swapi/service/PlanetService.java
@@ -0,0 +1,44 @@
+package com.ame.swapi.service;
+
+import com.ame.swapi.entity.PlanetEntity;
+import com.ame.swapi.repository.PlanetRepository;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PlanetService {
+
+    private final PlanetRepository planetRepository;
+
+    public PlanetService(PlanetRepository planetRepository) {
+        this.planetRepository = planetRepository;
+    }
+
+    @Transactional
+    public void save() {
+        //planetRepository.save()
+    }
+
+    public Iterable<PlanetEntity> findAllSalved() {
+        return planetRepository.findAll();
+    }
+
+    public Iterable<PlanetEntity> findAllAtStarWarsAPI() {
+        return null;
+    }
+
+    public Optional<PlanetEntity> findByName(String name) {
+        return planetRepository.findByName(name);
+    }
+
+    public Optional<PlanetEntity> findById(Long id) {
+        return planetRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        planetRepository.deleteById(id);
+    }
+
+}
