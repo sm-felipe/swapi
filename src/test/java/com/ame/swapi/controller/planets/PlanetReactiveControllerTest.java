3e9eafbdf9c0157915f5392461f4222989b43484
femello <femello@uolinc.com>
2019-08-24 20:00:15 -0300

    testando list

--- src/test/java/com/ame/swapi/controller/planets/PlanetReactiveControllerTest.java
+++ src/test/java/com/ame/swapi/controller/planets/PlanetReactiveControllerTest.java
@@ -4,6 +4,7 @@ import com.ame.swapi.client.PlanetGateway;
 import com.ame.swapi.model.dto.PlanetDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Before;
@@ -13,21 +14,23 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 //@WebFluxTest
 @RunWith(MockitoJUnitRunner.class)
 public class PlanetReactiveControllerTest {
 
-    MockWebServer mockWebServer = new MockWebServer();
+    private MockWebServer mockWebServer = new MockWebServer();
 
-    PlanetReactiveController planetReactiveController;
+    private PlanetReactiveController planetReactiveController;
 
-    ObjectMapper objectMapper = new ObjectMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Before
     public void init() {
-        WebClient client = WebClient.create(mockWebServer.url("/").toString());
-        planetReactiveController = new PlanetReactiveController(new PlanetGateway(client));
+        String baseUrl = mockWebServer.url("/").toString();
+        WebClient client = WebClient.create(baseUrl);
+        planetReactiveController = new PlanetReactiveController(new PlanetGateway(client, baseUrl));
     }
 
     @Test
@@ -44,5 +47,32 @@ public class PlanetReactiveControllerTest {
         PlanetDTO block = planetReactiveController.findById(1L).block();
         System.out.println(block);
 
+    }
+
+    @Test
+    public void listPlanets() throws JsonProcessingException {
+
+        PlanetDTO planetDTO = new PlanetDTO("name1", "clinamte", "dsfs", 1);
+        PlanetDTO planetDTO2 = new PlanetDTO("name2", "clinamte", "dsfs", 1);
+        List<PlanetDTO> planetList = List.of(planetDTO, planetDTO2);
+
+        //first page gives some elements
+        mockWebServer.enqueue(
+                new MockResponse()
+                .setResponseCode(200)
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_STREAM_JSON_VALUE)
+                .setBody(objectMapper.writeValueAsString(planetList))
+        );
+
+        //second page gives none so the flux can complete
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_STREAM_JSON_VALUE)
+                        .setBody(objectMapper.writeValueAsString(List.of()))
+        );
+
+        Flux<PlanetDTO> result = planetReactiveController.list();
+        result.subscribe(System.out::println);
     }
 }
