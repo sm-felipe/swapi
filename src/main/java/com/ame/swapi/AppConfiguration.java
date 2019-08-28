7657841c5a087374e7d847ea86359242f3c69bbe
femello <femello@uolinc.com>
2019-08-28 13:35:09 -0300

    configuracoes do swagger

--- src/main/java/com/ame/swapi/AppConfiguration.java
+++ src/main/java/com/ame/swapi/AppConfiguration.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ApiInfoBuilder;
+
 
 @Configuration
 @EnableAutoConfiguration
@@ -18,11 +26,31 @@ public class AppConfiguration {
 
     @Bean(name = "blockingAppClient")
     public WebClient blockingAppClient() {
-       return WebClient.create(blockingAppURL);
+        return WebClient.create(blockingAppURL);
     }
 
     @Bean(name = "swapiClient")
     public WebClient swapiClient() {
         return WebClient.create(swapiURL);
+    }
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enableUrlTemplating(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ps.account.association.api.rest"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Account Association API")
+                .description("Account Association API")
+                .version("1.0.0")
+                .build();
     }
 }
